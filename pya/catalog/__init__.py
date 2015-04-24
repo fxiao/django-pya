3ea06847530e9729056afb97c462cb3054fcfa69
fxiao <heyun51@gmail.com>
2015-04-24 15:18:48 +0800

    增加 分类模型

---
+++ pya/catalog/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# 产品相关
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4
---
+++ pya/catalog/models.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(u"名称", max_length=255)
+    parent = models.ForeignKey("self", verbose_name=u"父分类",
+                               blank=True, null=True)
+    image = models.ImageField(u"图片", upload_to="images", blank=True, null=True)
+
+    description = models.TextField(u"描述", blank=True, null=True)
+    meta_title = models.CharField(u"Meta Tag 标题", max_length=255)
+    meta_description = models.CharField(u"Meta Tag 描述", max_length=255)
+    meta_keyword = models.CharField(u"Meta Tag 关键字", max_length=255)
+
+    top = models.BooleanField(u"导航栏显示", default=True)
+    column = models.IntegerField(u"分类列排显示", default=0)
+    position = models.IntegerField(u"排序", default=1000)
+    active = models.BooleanField(u"状态", default=True)
+
+    date_added = models.DateTimeField(u"添加时间", auto_now_add=True)
+    date_modified = models.DateTimeField(u"修改时间", auto_now=True)
+
+    class Meta:
+        ordering = ("position",)
+        verbose_name = u"分类"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        if self.parent is not None:
+            return u"%s -> %s" % (self.parent.__unicode__(), self.name,)
+
+        return self.name
+# -------------------------------------------------- End class Category
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(u"名称", max_length=50, unique=True)
+    image = models.ImageField(u"图片", upload_to="images", blank=True, null=True)
+    position = models.IntegerField(u"排序", default=1000)
+
+    description = models.TextField(u"描述", blank=True, null=True)
+    meta_title = models.CharField(u"Meta Tag 标题", max_length=255)
+    meta_description = models.CharField(u"Meta Tag 描述", max_length=255)
+    meta_keyword = models.CharField(u"Meta Tag 关键字", max_length=255)
+
+    active = models.BooleanField(u"状态", default=True)
+
+    date_added = models.DateTimeField(u"添加时间", auto_now_add=True)
+    date_modified = models.DateTimeField(u"修改时间", auto_now=True)
+
+    class Meta:
+        ordering = ("position", "name", )
+        verbose_name = u"品牌"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
+# -------------------------------------------------- End class Manufacturer
